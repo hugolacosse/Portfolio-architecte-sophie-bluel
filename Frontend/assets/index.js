7cd6447b091b6541cf565fd7faf1a0c8fc4d781a
hugolacosse <hugolacosse.pro@gmail.com>
2025-07-01 11:25:27 +0200

    Add edit header/icon/span when user is logged in

--- Frontend/assets/index.js
+++ Frontend/assets/index.js
@@ -1,15 +1,28 @@
 import { initPortfolio } from "./modules/works.js"
 
-let token = window.localStorage.getItem("token");
-if (token) {
+let userToken = window.localStorage.getItem("token");
+if (userToken) {
     auth.innerText = "logout";
     auth.addEventListener("click", (event) => {
         event.preventDefault();
         window.localStorage.removeItem("token");
         window.location.href = 'index.html';
     });
+
+    displayEditHeader();
+    displayEditSpan();
 }
 
 initPortfolio();
 
 
+
+function displayEditHeader() {
+    const editHeader = document.querySelector(".edit-header")
+    editHeader.style.display = "flex";
+}
+
+function displayEditSpan() {
+    const editspan = document.querySelector(".portfolio-edit-span");
+    editspan.style.display = "inline";
+}
