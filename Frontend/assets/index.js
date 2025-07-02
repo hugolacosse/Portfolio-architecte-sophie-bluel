dfa96b5662c1192a284b4c38274388622d8bc63d
hugolacosse <hugolacosse.pro@gmail.com>
2025-07-02 10:28:35 +0200

    Add first modal and change edit anchor

--- Frontend/assets/index.js
+++ Frontend/assets/index.js
@@ -16,13 +16,42 @@ if (userToken) {
 initPortfolio();
 
 
-
+function displayEditModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "flex";
+    modal.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
 function displayEditHeader() {
     const editHeader = document.querySelector(".edit-header")
     editHeader.style.display = "flex";
+    editHeader.addEventListener("click", displayEditModal);
 }
 
 function displayEditSpan() {
-    const editspan = document.querySelector(".portfolio-edit-span");
-    editspan.style.display = "inline";
+    const editAnchor = document.querySelector(".portfolio-edit a");
+    editAnchor.style.display = "inline";
+    editAnchor.addEventListener("click", displayEditModal);
 }
+
+/*
+https://www.youtube.com/watch?v=EIC1_0Dfa9o&t=3s
+https://www.w3schools.com/howto/howto_css_modals.asp
+
+<aside id="modal" class="modal" aria-hidden="true" role="dialog">
+    <div class="modal-wrapper"></div>
+</aside>
+
+
+// addEventListenener -> click ->
+    - style         :display       : flex
+    - setAttribute  :aria-hidden   : true
+
+// global variable -> let modal = Element
+
+// se refermer au clic sur la croix ou en dehors de la modal
+
+*/
